f908896e686bd67117a4be7c42ad12ba228d903f
komatsu kana <kirakiraipai70@gmail.com>
2026-03-13 12:03:58 +0900

    ユーザー認証機能設定

---
+++ src/app/Actions/Fortify/CreateNewUser.php
@@ -0,0 +1,25 @@
+<?php
+
+namespace App\Actions\Fortify;
+
+use App\Models\User;
+use Illuminate\Support\Facades\Hash;
+use Illuminate\Support\Facades\Validator;
+
+class CreateNewUser
+{
+    public function create(array $input)
+    {
+        Validator::make($input, [
+            'name' => ['required','string','max:255'],
+            'email' => ['required','email','max:255','unique:users'],
+            'password' => ['required','confirmed','min:8'],
+        ])->validate();
+
+        return User::create([
+            'name'=>$input['name'],
+            'email'=>$input['email'],
+            'password'=>Hash::make($input['password']),
+        ]);
+    }
+}
--- src/app/Providers/RouteServiceProvider.php
+++ src/app/Providers/RouteServiceProvider.php
@@ -17,7 +17,7 @@ class RouteServiceProvider extends ServiceProvider
      *
      * @var string
      */
-    public const HOME = '/home';
+    public const HOME = '/moods';
 
     /**
      * The controller namespace for the application.
